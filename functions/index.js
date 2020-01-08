930b681a0bcbde72fced1439a7611f5ee1cdf235
Andrew Li <andruwuli@gmail.com>
2020-01-08 14:15:14 -0800

    Begin signup function

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,21 @@ admin.initializeApp();
 const express = require("express");
 const app = express();
 
+var firebaseConfig = {
+  apiKey: "AIzaSyBbYTrpq99j95loiHfk7-_M8qWY_V6Mvxo",
+  authDomain: "haiku-twitter.firebaseapp.com",
+  databaseURL: "https://haiku-twitter.firebaseio.com",
+  projectId: "haiku-twitter",
+  storageBucket: "haiku-twitter.appspot.com",
+  messagingSenderId: "853305919295",
+  appId: "1:853305919295:web:60f9bce28e1ae19867358a",
+  measurementId: "G-L6RKPLTGPD"
+};
+
+const firebase = require("firebase");
+firebase.initializeApp(firebaseConfig);
+
+// route for getting all haikus
 app.get("/get-haikus", (req, res) => {
   admin
     .firestore()
@@ -28,6 +43,7 @@ app.get("/get-haikus", (req, res) => {
     .catch(err => console.error(err));
 });
 
+// route for creating a haiku
 app.post("/create-haiku", (req, res) => {
   const newHaiku = {
     body: req.body.body,
@@ -48,5 +64,59 @@ app.post("/create-haiku", (req, res) => {
     });
 });
 
-// Creates multiple routes
+// signup route
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    userHandle: req.body.userHandle
+  };
+
+  let token, userId;
+  admin
+    .firestore()
+    .doc(`/users/${newUser.userHandle}`)
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        return res
+          .status(400)
+          .json({ userHandle: "this user handle is already taken!" });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then(data => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then(idToken => {
+      token = idToken;
+      const userCredentials = {
+        userHandle: newUser.userHandle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId: userId
+      };
+      admin
+        .firestore()
+        .doc(`/users/${newUser.userHandle}`)
+        .set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch(err => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email is already in use" });
+      }
+      return res.status(500).json({ error: err.code });
+    });
+});
+
+// Good practice to use Express to create multiple routes under /api/
 exports.api = functions.https.onRequest(app);
