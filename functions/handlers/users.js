dd23ebe2b98a8868d3cdc604cd7c5b477cd21ed3
Andrew Li <andruwuli@gmail.com>
2020-01-12 18:18:34 -0800

    Holy shit profile pics work

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -19,6 +19,8 @@ exports.signup = (req, res) => {
 
   if (!valid) return res.status(400).json(errors);
 
+  const noImage = "no-img.png";
+
   let token, userId;
   admin
     .firestore()
@@ -45,6 +47,7 @@ exports.signup = (req, res) => {
         userHandle: newUser.userHandle,
         email: newUser.email,
         createdAt: new Date().toISOString(),
+        imageURL: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImage}?alt=media`,
         userId: userId
       };
       admin
@@ -92,4 +95,60 @@ exports.login = (req, res) => {
       }
       return res.status(500).json({ error: err.code });
     });
+};
+
+exports.uploadImage = (req, res) => {
+  const BusBoy = require("busboy");
+  const path = require("path");
+  const os = require("os");
+  const fs = require("fs");
+
+  const busboy = new BusBoy({ headers: req.headers });
+
+  let imageToBeUploaded = {};
+  let imageFileName;
+
+  busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+    console.log(fieldname, file, filename, encoding, mimetype);
+    if (mimetype !== "image/jpeg" && mimetype !== "image/png") {
+      return res.status(400).json({ error: "Wrong file type submitted" });
+    }
+    // my.image.png => ['my', 'image', 'png']
+    const imageExtension = filename.split(".")[filename.split(".").length - 1];
+    // 32756238461724837.png
+    imageFileName = `${Math.round(
+      Math.random() * 1000000000000
+    ).toString()}.${imageExtension}`;
+    const filepath = path.join(os.tmpdir(), imageFileName);
+    imageToBeUploaded = { filepath, mimetype };
+    file.pipe(fs.createWriteStream(filepath));
+  });
+  busboy.on("finish", () => {
+    admin
+      .storage()
+      .bucket()
+      .upload(imageToBeUploaded.filepath, {
+        resumable: false,
+        metadata: {
+          metadata: {
+            contentType: imageToBeUploaded.mimetype
+          }
+        }
+      })
+      .then(() => {
+        const imageURL = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
+        return admin
+          .firestore()
+          .doc(`/users/${req.user.userHandle}`)
+          .update({ imageURL });
+      })
+      .then(() => {
+        return res.json({ message: "image uploaded successfully" });
+      })
+      .catch(err => {
+        console.error(err);
+        return res.status(500).json({ error: "something went wrong" });
+      });
+  });
+  busboy.end(req.rawBody);
 };
--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 const FBAuth = require("./util/fbAuth");
 const { getHaikus, createHaiku } = require("./handlers/haikus");
-const { signup, login } = require("./handlers/users");
+const { signup, login, uploadImage } = require("./handlers/users");
 
 // routes for haikus
 app.get("/get-haikus", getHaikus);
@@ -13,6 +13,7 @@ app.post("/create-haiku", FBAuth, createHaiku);
 // routes for users
 app.post("/signup", signup);
 app.post("/login", login);
+app.post("/user/image", FBAuth, uploadImage);
 
 // Good practice to use Express to create multiple routes under /api/
 exports.api = functions.https.onRequest(app);
