b90c7a73f5ebfb3f5abab8d4d4d2948aa326a16e
Andrew Li <andruwuli@gmail.com>
2020-01-16 17:53:16 -0800

    Login 500 err to 403 general

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -200,12 +200,9 @@ exports.login = (req, res) => {
     })
     .catch(err => {
       console.error(err);
-      // if (err.code === "auth/wrong-password") {
-      //   return res
-      //     .status(403)
-      //     .json({ general: "Wrong credentials, please try again" });
-      // }
-      return res.status(500).json({ error: err.code });
+      return res
+        .status(403)
+        .json({ general: "Wrong credentials, please try again" });
     });
 };
 
